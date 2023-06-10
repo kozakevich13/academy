41dffe1dcf801c2196ae07086443daa26a1a4c0e
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-06-10 21:46:09 +0300

    add bonus section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import "./Main.css";
 import globe from "../../img/globe.svg";
 import profile_img from "../../img/profile-img.png";
+import bonus_img from "../../img/bonus-img.svg";
 
 function Main() {
   return (
@@ -34,6 +35,13 @@ function Main() {
             6 лет коммерческого опыта с такими компаниями как Mercedes-Benz и
             другими крупными корпорациями
           </p>
+        </div>
+      </div>
+      <div className="bonus-section">
+        <img className="bonus-img" src={bonus_img} alt="profile-img" />
+        <div>
+          <h2>Бонус за регистрацию</h2>
+          <p>PDF-файл "5 преимуществ профессии фронтенд разработчика"</p>
         </div>
       </div>
     </div>
