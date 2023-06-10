55b4760450d460bece5289ff932efeca7ddf5270
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-06-10 20:42:07 +0300

    add block with text

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,6 +11,18 @@ function Main() {
       <div className="main-title">
         <p>front-end</p>
       </div>
+      <div className="first-decription">
+        <div className="container-zero">
+          <h2>с нуля</h2>
+        </div>
+        <p>легкий старт в IT профессии</p>
+      </div>
+      <div className="secont-decription">
+        Узнайте какими{" "}
+        <b>навыками должен обладать фронтенд разработчик в 2022 году</b> и как
+        начать карьеру в востребованной профессии <b>с зарплатой</b>{" "}
+        <mark>от 1 000$</mark>
+      </div>
     </div>
   );
 }
