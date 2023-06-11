1865034312f50dd4b6a0eb585ae651e741a1c455
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-06-11 12:10:21 +0300

    add form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./Form.css";
+
+const Form = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const inputs = [...e.target.elements];
+    const res = inputs.reduce((prev, current) => {
+      if (current.id) {
+        prev[current.id] = current.value;
+      }
+
+      return prev;
+    }, {});
+
+    const link = document.createElement("a");
+    link.href = `mailto:vitaliy.kozakevich@gmail.com?subject=name:${res.subject}&body=phone number:${res.description}, email: ${res.email}`;
+    link.click();
+  }
+
+  return (
+    <div className="user-form">
+      <form onSubmit={handleSubmit}>
+        <h2>Запишитесь бесплатно и получите подарок</h2>
+        <input id="email" type="text" placeholder="email" required />
+        <input
+          id="subject"
+          type="text"
+          placeholder="Імя та Прізвище"
+          required
+        />
+        <input id="description" placeholder="Номер телефону" />
+        <div>
+          <button type="submit">Записаться бесплатно</button>
+        </div>
+
+        <p>
+          Нажимая на кнопку я согашаюсь <br />{" "}
+          <u>с политикой конфидециальности</u>{" "}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,48 +2,53 @@ import "./Main.css";
 import globe from "../../img/globe.svg";
 import profile_img from "../../img/profile-img.png";
 import bonus_img from "../../img/bonus-img.svg";
+import Form from "../Form/Form";
 
 function Main() {
   return (
     <div className="main-content">
-      <div className="vebinar">
-        <img src={globe} alt="globe"></img>
-        <p>Вебинар</p>
-      </div>
-      <div className="main-title">
-        <p>front-end</p>
-      </div>
-      <div className="first-decription">
-        <div className="container-zero">
-          <h2>с нуля</h2>
+      <div className="info-conteiner">
+        <div className="vebinar">
+          <img src={globe} alt="globe"></img>
+          <p>Вебинар</p>
         </div>
-        <p>легкий старт в IT профессии</p>
-      </div>
-      <div className="secont-decription">
-        Узнайте какими{" "}
-        <b>навыками должен обладать фронтенд разработчик в 2022 году</b> и как
-        начать карьеру в востребованной профессии <b>с зарплатой</b>{" "}
-        <mark>от 1 000$</mark>
-      </div>
-      <div className="info-person">
-        <img src={profile_img} alt="profile-img" />
-        <div>
-          <h2>
-            Кирилл <b>КАСАТОНОВ</b>
-          </h2>
-          <p>
-            6 лет коммерческого опыта с такими компаниями как Mercedes-Benz и
-            другими крупными корпорациями
-          </p>
+        <div className="main-title">
+          <p>front-end</p>
+        </div>
+        <div className="first-decription">
+          <div className="container-zero">
+            <h2>с нуля</h2>
+          </div>
+          <p>легкий старт в IT профессии</p>
+        </div>
+        <div className="second-decription">
+          Узнайте какими{" "}
+          <b>навыками должен обладать фронтенд разработчик в 2022 году</b> и как
+          начать карьеру в востребованной профессии <b>с зарплатой</b>{" "}
+          <mark>от 1 000$</mark>
+        </div>
+        <div className="info-person">
+          <img src={profile_img} alt="profile-img" />
+          <div>
+            <h2>
+              Кирилл <b>КАСАТОНОВ</b>
+            </h2>
+            <p>
+              6 лет коммерческого опыта с такими компаниями как Mercedes-Benz и
+              другими крупными корпорациями
+            </p>
+          </div>
+        </div>
+        <div className="bonus-section">
+          <img className="bonus-img" src={bonus_img} alt="profile-img" />
+          <div>
+            <h2>Бонус за регистрацию</h2>
+            <p>PDF-файл "5 преимуществ профессии фронтенд разработчика"</p>
+          </div>
         </div>
       </div>
-      <div className="bonus-section">
-        <img className="bonus-img" src={bonus_img} alt="profile-img" />
-        <div>
-          <h2>Бонус за регистрацию</h2>
-          <p>PDF-файл "5 преимуществ профессии фронтенд разработчика"</p>
-        </div>
-      </div>
+
+      <Form />
     </div>
   );
 }
